bd810849b43ee49c8e5e225d2c6769cee12a8ce3
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2024-11-07 00:22:15 +0100

    Initial commit

---
+++ src/main/java/com/example/ea_project/EaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ea_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaProjectApplication.class, args);
+    }
+
+}
